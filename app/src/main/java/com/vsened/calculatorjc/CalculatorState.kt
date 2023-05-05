05f756225b29d1a503e89c925a61e8e285206351
vsened <vasily.senederov@gmail.com>
2023-05-05 13:25:00 +0300

    check

--- app/src/main/java/com/vsened/calculatorjc/CalculatorState.kt
+++ app/src/main/java/com/vsened/calculatorjc/CalculatorState.kt
@@ -1,3 +1,7 @@
 package com.vsened.calculatorjc
 
-data class CalculatorState()
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
+)
--- app/src/main/java/com/vsened/calculatorjc/CalculatorViewModel.kt
+++ app/src/main/java/com/vsened/calculatorjc/CalculatorViewModel.kt
@@ -1,4 +1,12 @@
 package com.vsened.calculatorjc
 
-class CalculatorViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel: ViewModel() {
+
+    var state by mutableStateOf(CalculatorState())
+        private set
 }
