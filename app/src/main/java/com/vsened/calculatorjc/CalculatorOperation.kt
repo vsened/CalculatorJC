f2ef4f9b790927fa3ce14e83c7c3c9bf1006edf3
vsened <vasily.senederov@gmail.com>
2023-05-05 16:00:00 +0300

    adding_ui_with_jc

--- app/src/main/java/com/vsened/calculatorjc/CalculatorOperation.kt
+++ app/src/main/java/com/vsened/calculatorjc/CalculatorOperation.kt
@@ -4,5 +4,5 @@ sealed class CalculatorOperation(val symbol: String) {
     object Add: CalculatorOperation("+")
     object Subtract: CalculatorOperation("-")
     object Multiply: CalculatorOperation("x")
-    object Divine: CalculatorOperation("/")
+    object Divide: CalculatorOperation("/")
 }
--- app/src/main/java/com/vsened/calculatorjc/MainActivity.kt
+++ app/src/main/java/com/vsened/calculatorjc/MainActivity.kt
@@ -10,6 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vsened.calculatorjc.ui.theme.CalculatorJCTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,6 +20,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CalculatorJCTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
 
             }
         }
